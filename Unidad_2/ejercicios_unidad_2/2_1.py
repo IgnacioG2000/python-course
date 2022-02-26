09054a06524290fe1670774db585f0996d3adedb
Ignacio Oscar García <nachog.2000@hotmail.com>
2022-02-26 17:44:36 -0300

    Mejoro expresion del ejercicio 2.1

--- Unidad_2/ejercicios_unidad_2/2_1.py
+++ Unidad_2/ejercicios_unidad_2/2_1.py
@@ -15,16 +15,16 @@ num3 = int(sys.argv[3])
 if num1 % 2 == 0 and num2 % 2 == 0 and num3 % 2 == 0:
     print("Los 3 numeros son multiplos de 2")
 elif num1 % 2 != 0 and num2 % 2 == 0 and num3 % 2 == 0:
-    print(f"{num1} no es multiplo de 2")
+    print(f"{num1} no es multiplo de 2 pero {num2} y {num3} lo son")
 elif num1 % 2 == 0 and num2 % 2 != 0 and num3 % 2 == 0:
-    print(f"{num2} no es multiplo de 2")
+    print(f"{num2} no es multiplo de 2 pero {num1} y {num3} lo son")
 elif num1 % 2 == 0 and num2 % 2 == 0 and num3 % 2 != 0:
-    print(f"{num3} no es multiplo de 2")
+    print(f"{num3} no es multiplo de 2 pero {num1} y {num2} lo son")
 elif num1 % 2 != 0 and num2 % 2 != 0 and num3 % 2 == 0:
-    print(f"{num1} y {num2} no son multiplos de 2")
+    print(f"{num1} y {num2} no son multiplos de 2 pero {num3} lo es")
 elif num1 % 2 == 0 and num2 % 2 == 0 and num3 % 2 != 0:
-    print(f"{num1} y {num3} no son multiplos de 2")
+    print(f"{num1} y {num3} no son multiplos de 2 pero {num2} lo es")
 elif num1 % 2 == 0 and num2 % 2 != 0 and num3 % 2 != 0:
-    print(f"{num2} y {num3} no son multiplos de 2")
+    print(f"{num2} y {num3} no son multiplos de 2 pero {num1} lo es")
 else:
     print("Ningun numero es multiplo de 2")
